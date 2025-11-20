e37e7418f161b4539ca9a09ab4a9a6f4d4a2116d
alfonso1410 <105889509+alfonso1410@users.noreply.github.com>
2025-11-20 02:50:04 -0600

    Cambios y bugs arreglados

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,19 +13,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // 1. Crear un Administrador (ADMIN)
-        DB::table('users')->updateOrInsert(
-            ['email' => 'renealdm3@gmail.com'], // Criterio de búsqueda (clave única)
-            [
-                'name' => 'Alfonso Rene',
-                'apellido_paterno' => 'Aldama',
-                'apellido_materno' => 'Trinidad',
-                'rol' => 'COORDINADOR', // Rol clave
-                'password' => Hash::make('Alfonso33'), // ¡IMPORTANTE! Contraseña encriptada
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]
-        );
-
+      
     }
 }
